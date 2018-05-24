c9e775dbb5b23ec9b4bf71cba73066290a29e3e4
CostaBlissPC\Sérgio Fernandes <sfernandes@blissapplications.com>
2018-05-24 13:38:34 +0100

    Fixed onClick event

--- src/static/main.js
+++ src/static/main.js
@@ -1,62 +1,51 @@
-(function () {
-  function keyDownHandler(e) {
-    var audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
-    var key = document.querySelector('div[data-key="' + e.keyCode + '"]');
+function playAudioOnClick(elem) {
+    var keyNumber = elem.dataset.key;
+    var audio = document.querySelector('audio[data-key="' + keyNumber + '"]');
+    var key = document.querySelector('div[data-key="' + keyNumber + '"]');
     removeAnimations();
+    // debugger;2222
     key.classList.add('bounce', 'animated');
     playSound(audio, key);
-  }
+}
 
-  function removeAnimations() {
-    var keys = Array.from(document.querySelectorAll(".soundboard__key"));
-    keys.forEach(function (key) {
-      return key.classList.remove('bounce', 'animated');
-    });
-  }
+function keyDownHandler(e) {
+  var audio = document.querySelector('audio[data-key="' + e.keyCode + '"]');
+  var key = document.querySelector('div[data-key="' + e.keyCode + '"]');
+  removeAnimations();
+  key.classList.add('bounce', 'animated');
+  playSound(audio, key);
+}
 
-  function clickHandler(e) {
-    var audio = document.querySelector('audio[data-key="' + this.dataset.key + '"]');
-    var key = document.querySelector('div[data-key="' + this.dataset.key + '"]');
-    playSound(audio, key);
-  }
-
-  function stopEveryone() {
-    var allAudios = document.getElementsByTagName('audio');
-    var allKeys = document.getElementsByClassName('soundboard__key');
-    for (var i = 0, len = allAudios.length; i < len; i++) {
-      allAudios[i].pause();
-      allAudios[i].currentTime = 0;
-    }
-    for (var i = 0, len = allKeys.length; i < len; i++) {
-      allKeys[i].classList.remove('bounce', 'animated');
-    }
-  }
-
-  function playSound(audio, key) {
-    stopEveryone();
-    audio.currentTime = 0;
-    audio.play();
-  }
-
+function removeAnimations() {
   var keys = Array.from(document.querySelectorAll(".soundboard__key"));
-
   keys.forEach(function (key) {
-    return key.addEventListener("click", clickHandler);
+    return key.classList.remove('bounce', 'animated');
   });
+}
 
-  window.addEventListener("keydown", keyDownHandler);
+function stopEveryone() {
+  var allAudios = document.getElementsByTagName('audio');
+  var allKeys = document.getElementsByClassName('soundboard__key');
+  for (var i = 0, len = allAudios.length; i < len; i++) {
+    allAudios[i].pause();
+    allAudios[i].currentTime = 0;
+  }
+  for (var i = 0, len = allKeys.length; i < len; i++) {
+    allKeys[i].classList.remove('bounce', 'animated');
+  }
+}
 
-  // document.addEventListener("play", function (e) {
-  //   var audios = document.getElementsByTagName("audio");
-  //   for (var i = 0, len = audios.length; i < len; i++) {
-  //     if (window.CP.shouldStopExecution(1)) {
-  //       break;
-  //     }
-  //     if (audios[i] != e.target) {
-  //       audios[i].pause();
-  //     }
-  //   }
-  //   window.CP.exitedLoop(1);
+function playSound(audio, key) {
+  stopEveryone();
+  audio.currentTime = 0;
+  console.log('asd');
+  audio.play();
+}
 
-  // }, true);
-})();
+var keys = Array.from(document.querySelectorAll(".soundboard__key"));
+
+keys.forEach(function (key) {
+  return key.addEventListener("click", clickHandler);
+});
+
+window.addEventListener("keydown", keyDownHandler);
